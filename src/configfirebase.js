cad10ebaabc51a64fa4c04ced233b31c5ad5984f
oherna20 <orazalhernandez.oh@gmail.com>
2021-03-09 10:48:20 -0600

    Added Firebase Config

---
+++ src/configfirebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCfpsfGa0KHH4_Zsj9YeYjzezu3MmY0W58",
+    authDomain: "loginexample-80fd9.firebaseapp.com",
+    projectId: "loginexample-80fd9",
+    storageBucket: "loginexample-80fd9.appspot.com",
+    messagingSenderId: "700864403418",
+    appId: "1:700864403418:web:25473454a68f3b00ef12e3"
+  };
